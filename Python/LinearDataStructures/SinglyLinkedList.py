6d3557c16061b50d56677d2cef4d240c14ec9c03
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-26 10:56:33 +0530

    search and length of singly linked list

--- Python/LinearDataStructures/SinglyLinkedList.py
+++ Python/LinearDataStructures/SinglyLinkedList.py
@@ -1,10 +1,10 @@
 class Node:
     def __init__(self, data):
-        self.head = None
         self.data  = data
         self.next = None
 
 class LinkedList:
+
     def __init__(self):
          self.head = None
     
@@ -30,16 +30,48 @@ class LinkedList:
         newNode.next = currentNode.next
         currentNode.next = newNode
     
+    def __findLinkedList(self, data):
+        currentNode = self.head
+        position = 1
+        l = []
+        while(currentNode):
+            if currentNode.data==data:
+                l.append(position)
+            position +=1
+            currentNode = currentNode.next
+        return l
+
+    def searchLinkedList(self, data):
+        p = self.__findLinkedList(data)
+        if p!=None:
+            print(f"Found at position: {p}")
+        else:
+            print("Not available!")
+    
+    def lengthLinkedList(self):
+        currentNode = self.head
+        count = 0
+        while(currentNode!=None):
+            count+=1
+            currentNode = currentNode.next
+        return count
+    
     def displayLinkedList(self):
         print("Linked List: ")
         currentNode = self.head
-        while(currentNode):
+        while(currentNode!=None):
             print(currentNode.data)
             currentNode = currentNode.next
+            
+
+        
 
 l = LinkedList()
 l.insertAtHead(10)
 l.insertAtHead(20)
-l.insertAtTail(1000)
-l.insertAtPosition(69, 2)
+l.insertAtPosition(90, 2)
+l.insertAtTail(20)
 l.displayLinkedList()
+l.searchLinkedList(20)
+l1 = l.lengthLinkedList()
+print(l1)
