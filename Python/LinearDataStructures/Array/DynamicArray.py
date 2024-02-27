a7894fcc1ab6354f44c40b3a244f79f6d3462ef8
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-27 13:41:04 +0530

    dynamic arrays

---
+++ Python/LinearDataStructures/Array/DynamicArray.py
@@ -0,0 +1,38 @@
+class DynamicArray:
+
+    def __init__(self):
+        self.arr = []
+    
+    #? insert elements in dynamic array
+    def insertElement(self, data):
+        self.arr.append(data)
+    
+    #? display elements of dynamic array
+    def show(self):
+        for i in self.arr:
+            print(i, end=" ")
+    
+    #* private 
+    def __findElement(self, data):
+        l = []
+        for i in range(len(self.arr)):
+            if self.arr[i]==data:
+                l.append(i)
+        return l
+    
+    #? search elements in the array:
+    def searchElement(self, data):
+        i = self.__findElement(data)
+        if i==[]:
+            print(f"\n{data}, not found in the array")
+        else:
+            print(f"\nFound at indexes: {i}")
+
+d = DynamicArray()
+d.insertElement(10)
+d.insertElement(20)
+d.insertElement(800)
+d.insertElement(40)
+d.insertElement(50)
+d.show()
+d.searchElement(900)
