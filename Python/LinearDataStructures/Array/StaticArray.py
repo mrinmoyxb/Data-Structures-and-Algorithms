34396effa07ccdab1da5088aa5735050fec75fd6
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-27 11:56:17 +0530

    search and replace elements in array

--- Python/LinearDataStructures/Array/StaticArray.py
+++ Python/LinearDataStructures/Array/StaticArray.py
@@ -4,20 +4,22 @@ class Array:
         self.size = size
         self.arr = []
     
+    #? insert an element in the array: 
     def insertElement(self, data):
         if(self.size-len(self.arr)>=1):
             self.arr.append(data)
         else:
             print(f"Can't insert {data} in the given array, out of range")
     
+    #? delete an element from the array:
     def deleteElement(self, data):
-        for i in range(len(self.arr)):
-            if self.arr[i]==data:
-                self.arr[i] = ""
+        for i in self.arr:
+            if i==data:
+                self.arr.remove(i)
                 return True
-            else:
-                return False
-            
+        return False
+
+    #* private 
     def __findElement(self, data):
         l = []
         for i in range(len(self.arr)):
@@ -25,14 +27,31 @@ class Array:
                 l.append(i)
         return l
     
+    #? search elements in the array:
     def searchElement(self, data):
         i = self.__findElement(data)
         if i==[]:
             print("Not found")
         else:
             print(f"Found at indexes: {i}")
-        
     
+    #* private 
+    def __replaceElement(self, index, value):
+        if index>0 and index<=self.size-1:
+            for i in range(len(self.arr)):
+                if i==index:
+                    self.arr[i]=value
+                    return True
+    
+    #? replace an element in the array based on index:
+    def searchAndReplace(self, index, value):
+        s = self.__replaceElement(index, value)
+        if s==True:
+            print("Replaced")
+        else:
+            print("Index not found")
+        
+    #? display the array:
     def show(self):
         print("Array: ")
         for i in range(len(self.arr)):
@@ -40,14 +59,14 @@ class Array:
     
 
 a = Array(5)
-a.insertElement(1)
+a.insertElement(10)
 a.insertElement(20)
-a.insertElement(100)
-a.insertElement(90)
-a.insertElement(100)
+a.insertElement(30)
+a.insertElement(40)
+a.insertElement(50)
 a.show()
 print()
-#a.insertElement(10000)
-#print(a.deleteElement(90))
-a.searchElement(100)
+
+a.searchAndReplace(2, 900)
 a.show()
+
--- Python/LinkedList/p.py
+++ Python/LinkedList/p.py
@@ -1,27 +1,24 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+def demo(index, data):
+    l = [12, 13, 45, 60, 43]
+    size = 5
+    print(l)
+    if index>0 and index<=size-1:
+        for i in range(len(l)):
+            if i==index:
+                l[i] = data
+                print("Replaced")
+    print(l)
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+demo(2, 900)
 
-    
-    def insertAtHead(self, data):
-        newNode = Node(data)
-        newNode.next = self.head
-        self.head = newNode
-    
-    def display(self):
-        currentNode = self.head
-        while(currentNode):
-            print(f"currentNode->data:  {currentNode.data}")
-            currentNode = currentNode.next
 
-n = LinkedList()
-n.insertAtHead("A")
-n.insertAtHead(20)
-n.insertAtHead("V")
-n.insertAtHead(45.6)
-n.display()
+# def demos(data):
+#   l = [12, 13, 45, 60, 43]
+#   print(l)
+
+#   copy_l = l[:]  # Create a copy of the list
+#   for item in copy_l:  # Loop through the copy
+#     if item == data:
+#       l.remove(item)  # Remove the item from the original list
+
+# demo(45)
