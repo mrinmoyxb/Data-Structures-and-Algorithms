c9aada78c03e312458ee9619f89bc560dac7a5ef
mrinmoyxb <mrinmoyborah26100@gmail.com>
2024-06-19 23:39:33 +0530

    search in hash table

--- NeetCodeRoadmap/01_Hashing/00_SimpleHashing.cpp
+++ NeetCodeRoadmap/01_Hashing/00_SimpleHashing.cpp
@@ -11,6 +11,8 @@ int hashFunction(int value, int size){
 int main(){
     vector<int> v;
     bool enterElement = true;
+    int value;
+    const int N = 10; //* size of hash table
 
     cout<<"Enter the elements in the array: "<<endl;
     while(enterElement){
@@ -25,14 +27,15 @@ int main(){
             enterElement = false;
         }
     }
+    cout<<endl;
 
     cout<<"Elements in the array are: "<<endl;
     for(int i=0; i<v.size(); i++){
         cout<<v[i]<<" ";
     }
+    cout<<endl;
 
     //! Hash Table : generate the hash value and store the element in that index(index = hash value)
-    const int N = 10;
     vector<int> hashTable(10, -1);
     for(int i=0; i<v.size(); i++){
         int hashValue = hashFunction(v[i], N);
@@ -44,5 +47,17 @@ int main(){
         cout<<i<<" : "<<hashTable[i]<<endl;
     }
 
+    //! Search in hash table at O(1):
+    cout<<"\nEnter value you want to search: ";
+    cin>>value;
+    int hashValue = hashFunction(value, N);
+    cout<<"Hash value genearted: "<<hashValue<<endl;
+    cout<<"Value at index(hash value): "<<hashTable[hashValue]<<endl;
+    if(hashTable[hashValue] == value){
+        cout<<"Value is present in the hash table"<<endl;
+    }
+    else{
+        cout<<"Not available in the hash table"<<endl;
+    }
     return 0;
 }
