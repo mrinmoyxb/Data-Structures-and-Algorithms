851d76935846ad9d4b9ff4bbb736e0eb9d73f499
mrinmoyxb <mrinmoyborah26100@gmail.com>
2024-06-19 20:57:18 +0530

    Queries

---
+++ NeetCodeRoadmap/00_Vectors/09_Queries.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    const int N = 1e5 + 10; /* exponent in C++ 10^5*/
+    vector<int> v(N, 0); /* size N and all values are initialised to 0*/
+    int size, queries;
+
+
+    cout<<"Enter the size of the array: ";
+    cin>>size;
+
+    cout<<"Enter the elements: "<<endl;
+    for(int i=0; i<size; i++){
+        int element;
+        cin>>element;
+        vec.push_back(element);
+    }
+
+    cout<<"The array is: "<<endl;
+    for(int i=0; i<vec.size(); i++){
+        cout<<vec[i]<<" ";
+    }
+
+    //! Main Section: 
+    for(int i=0; i<size; i++){
+        v[vec[i]]++; /*v[vec[i]] = v[vec[i]] + 1*/
+    }
+    //! 
+
+    cout<<"\nEnter the number of queries: ";
+    cin>>queries;
+
+    while(queries--){
+        int query;
+        cout<<"\nEnter query: ";
+        cin>>query;
+        cout<<"Number of occurence: "<<v[query]<<endl;
+    }
+
+    return 0;
+}
