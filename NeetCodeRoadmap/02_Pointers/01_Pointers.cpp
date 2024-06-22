40efc19b73da26827e797997971dcd70245a0dac
mrinmoyxb <mrinmoyborah26100@gmail.com>
2024-06-22 23:04:16 +0530

    Array and Pointers

--- NeetCodeRoadmap/02_Pointers/01_Pointers.cpp
+++ NeetCodeRoadmap/02_Pointers/01_Pointers.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
+void arrayPointer(){
     int array1[5] = {10, 20, 30, 40, 50};
 
     cout<<"Array: "<<endl;
@@ -31,8 +30,34 @@ int main(){
     cout<<"Value at array1[0]: "<<*(array1)<<endl;;
     cout<<"Value at array1[0]: "<<*ptr<<endl;
     cout<<"Address of *ptr: "<<&ptr<<endl;
+}
 
 
+void arrayPointer2(){
+    int arr[5] = {102, 103, 104, 105, 106};
+
+    cout<<"Concept 1: "<<endl;
+    int *ptr = arr;
+    cout<<"*ptr: "<<*ptr<<endl;
+    cout<<"arr[0]: "<<arr[0]<<endl;
+    cout<<"ptr: "<<ptr<<endl;
+    cout<<"arr: "<<arr<<endl;
+
+    cout<<"\nConcept 2: "<<endl;
+    cout<<"arr[1]: "<<arr[1]<<endl;
+    cout<<"*(arr + 1): "<<*(arr + 1)<<endl;
+    cout<<"*(ptr + 1): "<<*(ptr + 1)<<endl;
+    cout<<"arr[1] + 10: "<< (*(arr + 1) + 10)<<endl;
+
+    cout<<"\nConcept 3: "<<endl;
+    cout<<"Address of arr[2]: "<<&arr[1]<<endl;
+    cout<<"Address of arr[2]: "<<(arr + 1)<<endl;
+
+}
+
+
+int main(){
+    arrayPointer2();
     return 0;
 }
 
