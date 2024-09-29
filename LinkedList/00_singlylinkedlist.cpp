1e58192f4db2830c7dd7ad07e9a1fdd88f8693d0
mrinmoyxb <mrinmoyborah26100@gmail.com>
2024-09-29 23:31:52 +0530

    insert at position linked list

--- LinkedList/00_singlylinkedlist.cpp
+++ LinkedList/00_singlylinkedlist.cpp
@@ -26,6 +26,18 @@ void insertAtTail(Node* &head, int data){
     current->next = newNode;
 }
 
+void insertAtMid(Node* &head, int data, int position){
+    Node* current = head;
+    Node* newNode = new Node(data);
+    int count = 1;
+    while(count<position-1){
+        current = current->next;
+        count++;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
 void display(Node* head){
     Node* current = head;
     cout<<"Linked List: "<<endl;
@@ -42,6 +54,9 @@ int main(){
     insertAtHead(head, 300);
     insertAtHead(head, 400);
     insertAtTail(head, 1000);
+    display(head);
+    cout<<endl;
+    insertAtMid(head, 1000, 3);
     display(head);
     return 0;
 }
