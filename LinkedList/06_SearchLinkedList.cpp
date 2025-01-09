29f4d524c026838c1fa7a77585be3e12d52b1037
mrinmoyxb <mrinmoyborah26100@gmail.com>
2025-01-09 23:42:19 +0530

    binary search in linked list

--- LinkedList/06_SearchLinkedList.cpp
+++ LinkedList/06_SearchLinkedList.cpp
@@ -45,13 +45,52 @@ void linearSearch(Node* &head, int key){
     }
 }
 
+Node* findMid(Node* head, Node* last){
+    Node* slow = head;
+    Node* fast = slow->next;
+    while(fast!=last){
+        fast = fast->next;
+        slow = slow->next;
+        if (fast != last) {
+            fast = fast->next;
+        }
+    }
+    return slow;
+}
+
+//! Binary search in linked list
+bool binarySearch(Node* head, int key){
+    Node* start = head;
+    Node* end = NULL;
+    Node* mid = findMid(head, end);
+
+    while(true){
+        if(mid==NULL){
+            return false;
+        }
+
+        if(mid->data == key){
+            return true;
+        }
+
+        if(key > mid->data){
+            start = mid->next;
+        }
+        else{
+            end = mid;
+        }
+        mid = findMid(head, end);
+    }
+    return false;
+}
+
 int main(){
-    Node* node = new Node(100);
+    Node* node = new Node(1000);
     Node* head = node;
-    insertAtHead(head, 200);
-    insertAtHead(head, 300);
+    insertAtHead(head, 800);
+    insertAtHead(head, 700);
     insertAtHead(head, 400);
-    insertAtHead(head, 1000);
+    insertAtHead(head, 100);
     display(head);
 
     int key;
@@ -59,4 +98,6 @@ int main(){
     std::cin>>key;
 
     linearSearch(head, key);
+
+    std::cout<<"\nBinary search: "<<binarySearch(head, key)<<std::endl;
 }
