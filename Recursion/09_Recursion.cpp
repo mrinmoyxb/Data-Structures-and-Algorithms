12ff8908168e294d835d0e30d8d7662ee39a013e
mrinmoyxb <mrinmoyborah26100@gmail.com>
2025-01-14 18:04:05 +0530

    binary search in linked list using recursion

--- Recursion/09_Recursion.cpp
+++ Recursion/09_Recursion.cpp
@@ -49,13 +49,50 @@ bool linearSearch(Node* head, int key){
     }
 }
 
+//! Mid element;
+Node* findMidElement(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast!=NULL && fast->next!=NULL){
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+//! Binary search using recursion
+bool binarySearch(Node* head, int key){
+    if(head==NULL){
+        return false;
+    }
+
+    Node* mid = findMidElement(head);
+    if(mid->data == key){
+        return true;
+    }
+
+    if(key > mid->data){
+        return binarySearch(mid->next, key);
+    }else{
+        Node* leftHead = head;
+        Node* temp = head;
+        while(temp->next!=mid){
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        bool result = binarySearch(leftHead, key);
+        return result;
+    }
+}
+
 int main(){
-    Node* node = new Node(100);
+    Node* node = new Node(600);
     Node* head = node;
-    insertAtHead(head, 200);
-    insertAtHead(head, 300);
-    insertAtHead(head, 400);
     insertAtHead(head, 500);
+    insertAtHead(head, 400);
+    insertAtHead(head, 300);
+    insertAtHead(head, 200);
+    insertAtHead(head, 100);
     displayWithoutRecursion(head);
 
     std::cout<<std::endl;
@@ -64,5 +101,15 @@ int main(){
 
     std::cout<<std::endl;
     std::cout<<"\nLinear search: "<<linearSearch(head, 200)<<std::endl;
+
+    std::cout<<std::endl;
+    Node* mid = findMidElement(head);
+    std::cout<<"Mid element: "<<mid->data<<std::endl;
+
+    std::cout<<std::endl;
+    int key;
+    std::cout<<"\nEnter key: ";
+    std::cin>>key;
+    std::cout<<"Binary search: "<<binarySearch(head, key)<<std::endl;
     return 0;
 }
