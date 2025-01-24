12c3bec9d01ffb0160f7d78a0987a4935b3fdf74
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-01-24 07:27:17 +0530

    Leetcode 706

---
+++ Hashing/706_HashMap.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+class MyHashMap{
+    std::vector<int> hashTable;
+    int size;
+
+    public:
+    MyHashMap(){
+        size = 1e6+1;
+        hashTable.resize(size);
+        fill(begin(hashTable), end(hashTable), -1);
+    }
+
+    void put(int key, int value){
+        hashTable[key] = value;
+    }
+
+    int get(int key){
+        return hashTable[key];
+    }
+
+    void remove(int key){
+        hashTable[key] = -1;
+    }
+};
+
+int main(){
+    MyHashMap myHashMap = MyHashMap();
+    myHashMap.put(1, 1); 
+    myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+    std::cout<<myHashMap.get(1)<<"\n";    // return 1, The map is now [[1,1], [2,2]]
+    std::cout<<myHashMap.get(3)<<"\n";    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+    myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+    std::cout<<myHashMap.get(2)<<"\n";    // return 1, The map is now [[1,1], [2,1]]
+    myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+    std::cout<<myHashMap.get(2)<<"\n";    // return -1 (i.e., not found), The map is now [[1,1]]
+    return 0;
+}
--- STL&Concepts/stlPractice.cpp
+++ STL&Concepts/stlPractice.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
 #include <list>
-using namespace std;
 
 void list1(){
-    std::list<int> l = {100, 200, 300, 400, 500};
-    for(const auto& value: l){
+    std::list<int> L = {10, 20, 30, 40, 50};
+    std::cout<<"Linked List: "<<"\n";
+    for(auto& value: L){
         std::cout<<value<<" ";
     }
 }
 
-int main(){
-    list1();
+void list2(){
+    std::list<int> numbers = {1, 2, 3, 4, 5};
+    std::cout<<"\nLinked List: "<<"\n";
+    for(int number: numbers){
+        std::cout<<number<<" ";
+    }
+}
+
+void list3(){
+    std::list<int> L = {10, 20, 30, 40, 50};
+    L.push_front(100);
+    L.push_back(1000);
+    for(auto& value: L){
+        std::cout<<value<<" ";
+    }
+    std::cout<<"\nFront: "<<L.front()<<"\n";
+    std::cout<<"Back: "<<L.back()<<"\n";
+    
+    L.pop_front();
+    L.pop_back();
+
+    for(auto& number: L){
+        std::cout<<number<<" ";
+    }
+}
+
+void list4(){
+    std::vector<std::list<int>> L = {{1, 2, 3}, {10, 20, 30}};
+    for(const auto& values: L){
+        for(const auto& value: values){
+            std::cout<<value<<" ";
+        }
+        std::cout<<"\n";
+    }
+}
+
+int main() {
+    list4();
     return 0;
 }
