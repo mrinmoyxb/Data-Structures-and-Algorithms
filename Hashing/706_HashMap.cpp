ea6ae7886e9a3340d04644a9706aa7afc4548a7b
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-01-25 19:20:59 +0530

    Hashmap approach 2

--- Hashing/706_HashMap.cpp
+++ Hashing/706_HashMap.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <list>
 
-class MyHashMap{
+//! Approach 1
+class MyHashMap1{
     private:
     std::vector<int> hashTable;
     int size;
@@ -11,7 +12,7 @@ class MyHashMap{
     }
 
     public:
-    MyHashMap(){
+    MyHashMap1(){
         size = 1e6+1;
         hashTable.resize(size);
         fill(begin(hashTable), end(hashTable), -1);
@@ -30,6 +31,59 @@ class MyHashMap{
     }
 };
 
+//! Approach 2
+class MyHashMap{
+    public:
+    std::vector<std::list<std::pair<int, int>>> buckets;
+    int size = 1e4;
+
+    MyHashMap(){
+        buckets.resize(size);
+    }
+
+    //! Put
+    void put(int key, int value){
+        int bucket_no = key%size;
+        auto& chain = buckets[bucket_no];
+        for(auto& it: chain){
+            if(it.first==key){
+                it.second = value;
+                return;
+            }
+        }
+        chain.emplace_back(key, value);
+    }
+
+    //! Get
+    int get(int key){
+        int bucket_no = key%size;
+        auto& chain = buckets[bucket_no];
+        if(chain.empty()){
+            return -1;
+        }
+        
+        for(auto& it: chain){
+            if(it.first==key){
+                return it.second;
+            }
+        }
+        return -1;
+        
+    }
+
+    //! Remove 
+    void remove(int key){
+        int bucket_no = key%size;
+        auto& chain = buckets[bucket_no];
+        for(auto it=chain.begin(); it!=chain.end(); it++){
+            if(it->first==key){
+                chain.erase(it);
+                return;
+            }
+        }
+    }
+
+};
 int main(){
     MyHashMap myHashMap = MyHashMap();
     myHashMap.put(1, 1); 
