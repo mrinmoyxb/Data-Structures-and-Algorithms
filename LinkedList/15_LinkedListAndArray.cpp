6ba8197a98cc50d33c80e97a9d28968b13c560d4
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-01-30 11:21:03 +0530

    removed single common element from list

--- LinkedList/15_LinkedListAndArray.cpp
+++ LinkedList/15_LinkedListAndArray.cpp
@@ -22,14 +22,9 @@ void insertAtTail(Node*& head, int data){
 }
 
 //! Common elements in array and linked list using hashmap
-std::vector<int> commonElementsInListAndArray(Node*& head){
-    std::vector<int> arr = {100, 200, 3, 4, 500, 600};
+std::vector<int> commonElementsInListAndArray(Node*& head, std::vector<int> arr){
+   
     std::map<int, int> hashTable;
-
-    std::cout<<"\nArray: "<<"\n";
-    for(auto value: arr){
-        std::cout<<value<<" ";
-    }
 
     Node* current = head;
     while(current!=NULL){
@@ -37,18 +32,10 @@ std::vector<int> commonElementsInListAndArray(Node*& head){
         current = current->next;
     }
 
-    std::cout<<"\nHash Table"<<"\n";
-    for(const auto& value: hashTable){
-        std::cout<<value.first<<"   "<<value.second<<"\n";
-    }
-
     std::vector<int> common;
-
-    std::cout<<"\nCommon elements: "<<"\n";
     for(int i=0; i<arr.size()-1; i++){
         if(hashTable[arr[i]]!=0){
             common.push_back(arr[i]);
-            std::cout<<arr[i]<<" ";
         }
     }
 
@@ -56,16 +43,32 @@ std::vector<int> commonElementsInListAndArray(Node*& head){
 }
 
 //! Removal of common elements
-void removalCommonElements(Node* &head, int data){
-    std::vector<int> result = commonElementsInListAndArray(head);
+void removalCommonElements(Node* &head, std::vector<int> arr){
+    std::vector<int> result = commonElementsInListAndArray(head, arr);
     Node* current = head;
     Node* prev = head;
     
-    // while(current!=NULL){
-    //     for(auto& value: result){
+    for(const auto& key: result){
+        if(head->data==key){
+            Node* delNode = head;
+            head = head->next;
+            delete delNode;
+        }else{
+            current = current->next;
+        }
 
-    //     }
-    // }
+        while(current!=NULL){
+            if(current->data==key){
+                Node* delNode = current;
+                current = current->next;
+                prev->next = current;
+                delete delNode;
+            }else{
+                prev = current;
+                current = current->next;
+            }
+        }
+    }
 }
 
 //! Deleting any element in linked list
@@ -111,15 +114,23 @@ int main(){
     Node* head = node;
     insertAtTail(head, 2);
     insertAtTail(head, 3);
-    insertAtTail(head, 4);
+    insertAtTail(head, 9);
     insertAtTail(head, 5);
     insertAtTail(head, 2);
     display(head);
     
-    deleteELement(head, 2);
+    std::vector<int> arr = {100, 200, 3, 4, 500, 600};
+    std::cout<<"\nArray: "<<"\n";
+    for(const auto& item: arr){
+        std::cout<<item<<" ";
+    }
+    std::vector<int> result = commonElementsInListAndArray(head, arr);
+    std::cout<<"\nCommon elements: "<<"\n";
+    for(const auto& value: result){
+        std::cout<<value<<" ";
+    }
+    removalCommonElements(head, arr);
     display(head);
-    // deleteELement(head, 4);
-    // deleteELement(head, 4);
 
     return 0;
 }
