d4d86557902a263ca033484a9ed8a41e2e0792e8
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-06-15 21:41:57 +0530

    vectors

--- DSA-2.0/Array/01_linearSearch.cpp
+++ DSA-2.0/Array/01_linearSearch.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//! linear search
 bool linearSearch(int arr[], int size, int key){
     for(int i=0; i<size; i++){
         if(arr[i]==key){
@@ -9,9 +10,26 @@ bool linearSearch(int arr[], int size, int key){
     return false;
 }
 
+//! reverse an array
+void reverseArray(int* arr, int size){
+    int start = 0;
+    int end = size-1;
+    while(start<=end){
+        std::swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    std::cout<<"\nReverse array: "<<std::endl;
+    for(int i=0; i<size; i++){
+        std::cout<<arr[i]<<" ";
+    }
+}
+
 int main(){
     int arr[5] = {100, 120, 150, 200, 300};
     int key = 150;
     std::cout<<"Linear Search: "<<linearSearch(arr, 5, key)<<std::endl;
+    reverseArray(arr, 5);
     return 0;
 }
---
+++ DSA-2.0/Array/02_vectors.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+//! Iterate vectors
+void vec1(){
+    std::vector<int> v1 = {10, 20, 30, 40, 50};
+
+    std::cout<<"\nIteration :"<<std::endl;
+    for(const int& i : v1){
+        std::cout<<i<<" ";
+    }
+
+    std::cout<<"\nIteration using iterator: "<<std::endl;
+    std::vector<int>::iterator iter;
+    for(iter = v1.begin(); iter!=v1.end(); ++iter){
+        std::cout<<*iter<<" ";
+    }
+
+    std::cout<<"\nIteration: "<<std::endl;
+    for(int i: v1){
+        std::cout<<i<<" ";
+    }
+}
+
+//! Modification of vectors;
+void vec2(){
+    std::vector<int> vec = {10, 20, 30, 40, 50};
+    std::cout<<"Vector: "<<std::endl;
+    for(const int& i:vec){
+        std::cout<<i<<" ";
+    }
+
+    std::cout<<"\nPushing two elements 2 and 4: "<<std::endl;
+    vec.push_back(2);
+    vec.push_back(4);
+    std::cout<<"\nVector: "<<std::endl;
+    for(const int& i: vec){
+        std::cout<<i<<" ";
+    }
+
+    std::cout<<"\nAccessing element at index 3: "<<vec.at(3)<<std::endl;
+    std::cout<<"\nUpdating element at index 3: "<<std::endl;
+    vec.at(3) = 10000;
+    std::cout<<"\nVector: "<<std::endl;
+    for(const int& i: vec){
+        std::cout<<i<<" ";
+    }
+
+    std::cout<<"\nRemove the last element: "<<std::endl;
+    vec.pop_back();
+}
+
+
+int main(){
+    vec2();
+    return 0;
+}
