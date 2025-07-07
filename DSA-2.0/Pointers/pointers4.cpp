aead76d70085af1af229b06cefdc57705fe470ad
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-07-07 18:06:37 +0530

    pointers with functions

--- DSA-2.0/Pointers/pointers4.cpp
+++ DSA-2.0/Pointers/pointers4.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 
+//! KEY CONCEPT:
+//! Function arguments are passed by value, even with pointers. The address is a copy.
+//* 1. In C++, function arguments are passed by value, even if we're passing a pointer, 
+//* the pointer itself (i.e. the address) is passed by value.
+
+//* 2. Here, ptr is a local copy of the address, because of which if we increment address by 1,
+//* it's visible only inside the function not outside.
+
+//* 3. *ptr inside function, Dereferences and modifies actual memory because even though function receives 
+//* a copy of the address, it's still a valid address and we can access and modify it using *ptr.
+
 void pointers1(int* ptr){
+    ptr = ptr+1;
+    *ptr+=10;
+}
+
+//! Modifying a value using *ptr inside the function
+void pointers2(int* ptr){
     std::cout<<"\nInside Function: "<<std::endl;
     std::cout<<"Accessing value using *ptr: "<<*ptr<<std::endl;
     std::cout<<"Accessing address using ptr: "<<ptr<<std::endl;
     *ptr += 10;
 }
 
-void pointers2(int* cptr){
+void pointers3(int* cptr){
     std::cout<<"\nInside Function: "<<std::endl;
     std::cout<<"Accessing value using *ptr: "<<*cptr<<std::endl;
     std::cout<<"Accessing address using ptr: "<<cptr<<std::endl;
