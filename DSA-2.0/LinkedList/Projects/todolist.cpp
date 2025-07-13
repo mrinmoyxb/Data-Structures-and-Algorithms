9d480b4dad5fb8f1a04983c2d68993460003492c
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-07-13 10:55:33 +0530

    completed todo list using linked list

--- DSA-2.0/LinkedList/Projects/todolist.cpp
+++ DSA-2.0/LinkedList/Projects/todolist.cpp
@@ -6,6 +6,7 @@ class Task{
     int id;
     std::string title;
     std::string description;
+    bool isTaskCompleted;
     Task* prev;
     Task* next;
 
@@ -13,6 +14,7 @@ class Task{
         this->id = id;
         this->title = title;
         this->description = descpription;
+        this->isTaskCompleted = false;
         this->prev = NULL;
         this->next = NULL;
     }
@@ -29,21 +31,16 @@ class ToDoList{
         Task* newTask = new Task(noOfTask+1, title, description);
         if(head==NULL){
             head = tail = newTask;
-            noOfTask += 1;
         }else{
-            Task* current = head;
-            while(current->next != NULL){
-                current = current->next;
-            }
-            current->next = newTask;
-            newTask->prev = current;
+            tail->next = newTask;
+            newTask->prev = tail;
             tail = newTask;
-            noOfTask += 1;
         }
+        noOfTask += 1;
     }
 
     void stats(){
-        std::cout<<"No. of tasks: "<<noOfTask<<std::endl;
+        std::cout<<"\nNo. of tasks: "<<noOfTask<<std::endl;
         std::cout<<"No. of completed tasks: "<<noOfCompletedTask<<std::endl;
         std::cout<<"No. of remaining tasks: "<<(noOfTask - noOfCompletedTask)<<std::endl;
     }
@@ -51,34 +48,71 @@ class ToDoList{
     void showTasks(){
         Task* current = head;
         while(current!=NULL){
-            std::cout<<"Task: "<<current->id<<std::endl;
+            std::cout<<"\nTask: "<<current->id<<std::endl;
             std::cout<<"Title: "<<current->title<<std::endl;
             std::cout<<"Description: "<<current->description<<std::endl;
-            std::cout<<std::endl;
+            std::string isTaskComplete = (current->isTaskCompleted == true) ? "Yes":"No";
+            std::cout<<"Completed: "<<isTaskComplete<<std::endl;
             current = current->next;
         }
     }
 
     void deleteTask(int id){
-        Task* current = head;
-        if(id==1){
-            Task* delTask = current;
-            current = current->next;
-            current->next->prev = NULL;
+        if(head!=NULL && head->id==id){
+            Task* delTask = head;
+            head = head->next;
+            if(head!=NULL){
+                head->prev = NULL;
+                head->id = 1;
+            }else{
+                tail = NULL;
+            }
             delete delTask;
+            noOfTask-=1;
+            return;
         }else{
+            Task* current = head;
             while(current!=NULL){
                 if(current->next->id==id){
-                    std::cout<<"Deleting task "<<current->next->id<<std::endl;
+                    std::cout<<"\nDeleted task "<<current->next->id<<std::endl;
                     Task* nextNode = current->next;
                     current->next = nextNode->next;
                     nextNode->prev = NULL;
                     delete nextNode;
+                    noOfTask-=1;
+                    Task* forward = current->next;
+
+                    while(forward!=NULL){
+                        forward->id-=1;
+                        forward=forward->next;
+                    }
                     break;
                 }else{
                     current = current->next;
                 }
             }
+        }
+    }
+
+    void completeTask(int id){
+        Task* current = head;
+        while(current!=NULL){
+            if(current->id==id){
+                current->isTaskCompleted = true;
+                noOfCompletedTask+=1;
+                break;
+            }else{
+                current = current->next;
+            }
+        }
+    }
+
+    ~ToDoList(){
+        Task* current = head;
+        while(current!=NULL){
+            Task* nextTask = current->next;
+            delete current;
+            current = nextTask;
         }
     }
 };
@@ -89,9 +123,18 @@ int main(){
     l.addTask("Gym", "Chest day");
     l.addTask("Shopping", "Need to buy something for her");
     l.addTask("Report", "Need to sumbit the report to HR by 10pm");
+     l.addTask("Date", "Date with my muffin");
+    l.addTask("Gaming Session", "Gamming Session");
+    l.addTask("Meeting", "Imp. meeting tomorrow, so prepare ppt.");
     l.showTasks();
     l.stats();
+    l.completeTask(4);
+    l.completeTask(5);
+    l.completeTask(7);
     l.deleteTask(3);
     l.showTasks();
+    l.deleteTask(5);
+    l.showTasks();
+    l.stats();
     return 0;
 }
