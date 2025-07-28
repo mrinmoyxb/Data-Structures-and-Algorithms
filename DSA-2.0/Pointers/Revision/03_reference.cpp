0ae521653731864c49c678d904dae8bb59c434d2
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-07-28 23:27:13 +0530

    pointers

--- DSA-2.0/Pointers/Revision/03_reference.cpp
+++ DSA-2.0/Pointers/Revision/03_reference.cpp
@@ -48,7 +48,43 @@ void dynamicAll(){
 }
 
 void dynamicArr(){
+    int* arr = new int[5];
+    for(int i=0; i<5; i++){
+        std::cout<<"Enter element at index "<<i<<std::endl;
+        std::cin>>arr[i];
+    }
 
+    std::cout<<"Array: "<<std::endl;
+    for(int i=0; i<5; i++){
+        std::cout<<arr[i]<<" ";
+    }
+}
+
+void constPointers(){
+    // constant Pointer
+    int num = 100;
+    int* const ptr = &num;
+
+    std::cout<<"num: "<<num<<std::endl;
+    std::cout<<"ptr: "<<ptr<<std::endl;
+    std::cout<<"*ptr: "<<*ptr<<std::endl;
+    *ptr = 200;
+    std::cout<<"\nConstant Pointers Manipulation"<<std::endl;
+    std::cout<<"num: "<<num<<std::endl;
+    std::cout<<"ptr: "<<ptr<<std::endl;
+    std::cout<<"*ptr: "<<*ptr<<std::endl;
+    // ptr+=1: can't change address
+}
+
+void tempPointers(){
+    int num = 100;
+    int* ptr = &num;
+
+    std::cout<<"num: "<<num<<std::endl;
+    std::cout<<"*ptr: "<<*ptr<<std::endl;
+    ptr+=1;
+    std::cout<<"num: "<<num<<std::endl;
+    std::cout<<"*ptr: "<<*ptr<<std::endl;
 }
 
 int main(){
@@ -62,7 +98,13 @@ int main(){
 
     // dynamicAll();
 
-    MyClass* obj = new MyClass;
+    // MyClass* obj = new MyClass;
+
+    //dynamicArr();
+
+    // constPointers();
+
+    tempPointers();
 
     return 0;
 }
