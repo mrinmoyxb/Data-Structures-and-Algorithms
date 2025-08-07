a0e48bc36f1817dc57832bc7ac1b258839c5433d
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-08-07 18:59:14 +0530

    two pointers

--- DSA-2.0/TwoPointers/TwoPointers1.cpp
+++ DSA-2.0/TwoPointers/TwoPointers1.cpp
@@ -53,19 +53,43 @@ void evenOdd(std::vector<int> &vec){
     }
 }
 
+void arrangeNumbersUsingPtr(std::vector<int> vec){
+    int left = 0;
+    int right = vec.size()-1;
+    std::vector<int> ans_vec;
+    while(left<=right){
+        if(abs(vec[left])<abs(vec[right])){
+            ans_vec.push_back(vec[right]*vec[right]);
+            right--;
+        }else{
+            ans_vec.push_back(vec[left]*vec[left]);
+            left++;
+        }
+    }
+
+    std::cout<<"\narray: "<<std::endl;
+    std::reverse(ans_vec.begin(), ans_vec.end());
+    for(auto i:ans_vec){
+        std::cout<<i<<" ";
+    }
+
+}
+
 int main(){
     std::vector<int> vec = {0, 1, 0, 3, 12, 0};
     std::vector<int> vec1 = {1, 1, 0, 0, 1, 0, 1, 0};
     std::vector<int> vec2 = {1, 4, 6, 9, 13};
+    std::vector<int> vec3 = {-10, -3, 2, 4, 5};
     // for(auto num: vec){
     //     std::cout<<num<<" ";
     // }
 
     //moveZeroes(vec);
     //twoPointersApproach(vec1);
-    evenOdd(vec2);
-    for(auto num: vec2){
+    // evenOdd(vec2);
+    for(auto num: vec3){
         std::cout<<num<<" ";
     }
+    arrangeNumbersUsingPtr(vec3);
     return 0;
 }
