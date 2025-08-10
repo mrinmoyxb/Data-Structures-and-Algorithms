cd05bfbad5199410595c15fd13e2471dcfb4109a
mrinmoyborah <mrinmoyborah26100@gmail.com>
2025-08-11 00:43:21 +0530

    Tree Traversal

--- DSA-2.0/Trees/02_BinaryTree.cpp
+++ DSA-2.0/Trees/02_BinaryTree.cpp
@@ -30,6 +30,7 @@ Node* buildTree(){
     return root;
 }
 
+//! Number of nodes in a tree
 int numberOfNodes(Node* root){
     if(root==NULL){
         return 0;
@@ -40,8 +41,37 @@ int numberOfNodes(Node* root){
     return left + right + 1;
 }
 
+//! Height of a tree: number of edges between root node and leaf node (the longest path)
+int heightOfTree(Node* root){
+    if(root==NULL){
+        return -1;
+    }
+
+    int left = heightOfTree(root->left);
+    int right = heightOfTree(root->right);
+    return 1 + std::max(left, right);
+}
+
+//! Full Binary Tree/Proper Binary tree: Parent or internal node has either two or no children
+bool isFullBinaryTree(Node* root){
+    if(root==NULL){
+        return true;
+    }
+
+    if(root->left == NULL && root->right ==NULL){
+        return true;
+    }
+
+    if((root->left)&&(root->right)){
+        return (isFullBinaryTree(root->left) && isFullBinaryTree(root->right));
+    }
+    return false;
+}
+
 int main(){
     Node* root = buildTree();
-    std::cout<<"number of nodes: "<<numberOfNodes(root)<<std::endl;
+    // std::cout<<"\nnumber of nodes: "<<numberOfNodes(root)<<std::endl;
+    // std::cout<<"\nheight of tree: "<<heightOfTree(root)<<std::endl;
+    std::cout<<"Is the tree a full binary: "<<isFullBinaryTree(root)<<std::endl;
     return 0;
 }
