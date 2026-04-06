a378c0f322c4c84c947658dc7620a5bbcbb2ef60
mrinmoyborah <mrinmoyborah26100@gmail.com>
2026-04-06 23:55:07 +0530

    insertion sort

---
+++ SortingAlgorithms/insertion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int size){
+    for(int i=1; i<size; i++){
+        int key = arr[i];
+        int j = i-1;
+
+        while(j>=0 && key<arr[j]){
+            arr[j+1] = arr[j];
+            --j;
+        }
+        arr[j+1] = key;
+    }
+}
+
+int main(){
+    return 0;
+}
